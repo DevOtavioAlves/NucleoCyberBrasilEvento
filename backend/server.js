49661d39a1404bf5bc27bee4e2ce82c47f579371
Jhônata Tavares <jhonatasos2006@gmail.com>
2025-09-26 17:14:45 -0300

    feat: Backend e Server.js funcionais e adição do projeto Frontend com React

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API rodando 🚀");
+});
+
+app.listen(8081, () => console.log("Servidor rodando na porta 8081"));
