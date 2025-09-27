a22e4e5dd95335c3d55d8fbfdb874e804ab34a82
Jhônata Tavares <jhonatasos2006@gmail.com>
2025-09-27 20:27:04 -0300

    Feat: Server Updates

--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
+import { fileURLToPath } from 'url';
 import express from "express";
+import path from "path";
 
-app.set('view engine', 'ejs'); // Configurando o EJS como motor de visualização
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.json());
 
+app.set('view engine', 'ejs'); // Configurando o EJS como motor de visualização
+
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", (req, res) => {
-  res.send("API rodando 🚀");
+  res.render("pages/home"); // Renderizando a view Home.ejs
 });
 
 app.get("/Forum", (req, res) => {
